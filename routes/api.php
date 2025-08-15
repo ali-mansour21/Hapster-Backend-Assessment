57fc09346310e745f0cfa33d193cc516fdc68447
Ali Mansour <alialhadimansour21@gmail.com>
2025-08-15 18:24:46 +0300

    fix(prodcuts): change the data variable to paginator and add the api resource.

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\products\ProductController;
 
-
+Route::resource('products', ProductController::class);
