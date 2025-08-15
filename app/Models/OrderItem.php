4d80f771a6d42d01a84540836b6737d72bf353b1
Ali Mansour <alialhadimansour21@gmail.com>
2025-08-15 16:47:12 +0300

    feat(migrations): add the realtions between orders, order items, and products.

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     protected $fillable = ['order_id', 'product_id', 'qty', 'price'];
+
+    public function order()
+    {
+        $this->belongsTo(Order::class);
+    }
+    public function product()
+    {
+        $this->belongsTo(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = ['name', 'sku', 'price', 'stock'];
+
+    public function orderItems()
+    {
+        $this->hasMany(OrderItem::class);
+    }
 }
